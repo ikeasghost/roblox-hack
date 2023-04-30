b3250cb0f5e4de09a25711d51d2d441b3839cae2
ikea <114372231+ikeasghost@users.noreply.github.com>
2023-04-30 04:48:53 +0000

    Update gun nospread.lua
    fix stupid errors made by retarded vscode autocomplete

--- mm2/gun nospread.lua
+++ mm2/gun nospread.lua
@@ -1,9 +1,9 @@
 local hook
 local old_mathrandom = clonefunction(getrenv().math.random)
 
-hook = hookfunction(getgenv().math.random, function(...)
+hook = hookfunction(getrenv().math.random, function(...)
     if getfenv(2).script.Name == "KnifeLocal" then
         return 0
     end
-    return(old_mathrandom(...))
+    return old_mathrandom(...)
 end)
